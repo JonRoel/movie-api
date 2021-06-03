204558b5814035e4fd6630a6b481fbcbdb09fa6f
JonRoel <77934501+JonRoel@users.noreply.github.com>
2021-06-02 20:36:31 -0400

    Temp Removed auth on /movies endpoint

--- src/index.js
+++ src/index.js
@@ -79,7 +79,7 @@ Returns all movies
 */
 app.get(
   "/movies",
-  passport.authenticate("jwt", { session: false }),
+  /*passport.authenticate("jwt", { session: false }),*/
   (req, res) => {
     Movies.find()
       .then((movies) => {
